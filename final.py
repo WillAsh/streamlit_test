55cc55183a2dc5424c1f6cd4c0a60c530de2b441
Quispe Ochoa <jquispe@inside.com.pe>
2024-09-12 02:17:49 -0500

    asd

--- final.py
+++ final.py
@@ -4,7 +4,7 @@ import requests
 import pandas as pd
 import time
 params= st.query_params.to_dict()
-print(params)
+st.write(params.get("vectorStore"))
 def get_backend_models():
     url = 'https://cr-lab-qdrant-5xoqaolxlq-uc.a.run.app/model/get_all'
     datos = {
