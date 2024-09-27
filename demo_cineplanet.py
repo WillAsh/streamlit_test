18cd197e2fb111ad365bcf79c5eaa198d88e9f36
Quispe Ochoa <jquispe@inside.com.pe>
2024-09-27 16:14:11 -0500

    cineplanet

--- demo_cineplanet.py
+++ demo_cineplanet.py
@@ -3,6 +3,19 @@ import json
 import requests
 import time
 
+
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 def send_message(prompt):
     url = 'https://dev-ai-chatbot-cineplanet-yutgchy3pa-uc.a.run.app/conversation'
     metadata = {
